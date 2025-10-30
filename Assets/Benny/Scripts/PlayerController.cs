22f51637210ceda13850fe1e50152c7dc53fb015
NebilYusuf <nebilyusuf@outlook.com>
2025-10-30 17:14:54 -0500

    Jumping

--- Assets/Benny/Scripts/PlayerController.cs
+++ Assets/Benny/Scripts/PlayerController.cs
@@ -16,12 +16,20 @@ namespace UnityTutorial.PlayerControl
         [SerializeField] private float BottomLimit = 70f;
         [SerializeField] private float MouseSensitivity = 21.9f;
 
+        [SerializeField] private float JumpFactor = 260f;
+        [SerializeField] private float Dis2Ground = 0.8f;
+        [SerializeField] private LayerMask GroundCheck;
+
         private Rigidbody _playerRigidbody;
         private InputManager _inputManager;
         private Animator _animator;
+        private bool _grounded;
         private bool _hasAnimator;
         private int _xVelHash;
         private int _yVelHash;
+        private int _jumpHash;
+        private int _groundedHash;
+        private int _fallingHash;
 
         private float _xRotation;
 
@@ -39,10 +47,15 @@ namespace UnityTutorial.PlayerControl
 
             _xVelHash = Animator.StringToHash("X_Velocity");
             _yVelHash = Animator.StringToHash("Y_Velocity");
+            _jumpHash = Animator.StringToHash("Jump");
+            _groundedHash = Animator.StringToHash("Grounded");
+            _fallingHash = Animator.StringToHash("Falling");
         }
 
         private void FixedUpdate() {
+            SampleGround();
             Move();
+            HandleJump();
         }
         private void LateUpdate() {
             CamMovements();
@@ -81,6 +94,45 @@ namespace UnityTutorial.PlayerControl
 
             Camera.localRotation = Quaternion.Euler(_xRotation, 0 , 0);
             _playerRigidbody.MoveRotation(_playerRigidbody.rotation * Quaternion.Euler(0, Mouse_X * MouseSensitivity * Time.smoothDeltaTime, 0));
+        }
+
+        private void HandleJump()
+        {
+            if(!_hasAnimator) return;
+            if(!_inputManager.Jump) return;
+            _animator.SetTrigger(_jumpHash);
+        }
+
+        public void JumpAddForce()
+        {
+            _playerRigidbody.AddForce(_playerRigidbody.linearVelocity.y * Vector3.up, ForceMode.VelocityChange);
+            _playerRigidbody.AddForce(Vector3.up * JumpFactor, ForceMode.Impulse);
+            _animator.ResetTrigger(_jumpHash);
+        }
+
+        private void SampleGround()
+        {
+            if(!_hasAnimator) return;
+
+            RaycastHit hitInfo;
+            if(Physics.Raycast(_playerRigidbody.worldCenterOfMass, Vector3.down, out hitInfo, Dis2Ground + 0.1f, GroundCheck))
+            {
+                //collided with sth
+                //Grounded
+                _grounded = true;
+                SetAnimationGrounding();
+                return;
+            }
+            Debug.Log(_grounded);
+            _grounded = false;
+            SetAnimationGrounding();
+            return;
+        }
+
+        private void SetAnimationGrounding()
+        {
+            _animator.SetBool(_fallingHash, !_grounded);
+            _animator.SetBool(_groundedHash, _grounded);
         }
 
     }
